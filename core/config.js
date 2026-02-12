f4b553411b6c00d6b291ae7d34498d831e7c936c
PedroPaesPorto <henriquepaesporto@gmail.com>
2026-02-12 11:45:29 -0400

    Create config.js

---
+++ core/config.js
@@ -0,0 +1,6 @@
+export const CONFIG = {
+        API_URL: "https://script.google.com/macros/s/AKfycbwNIRDBmERBDy0jtyCa4oXEChPM9ErwBze2oWkbN4w3eocoecJwQyTVw2z9dI8k3zAL6w/exec",
+        VERSAO: "5.0.1",
+        EMAIL_LIST: ["gmail.com","hotmail.com","outlook.com","yahoo.com","pm.ro.gov.br"],
+        ADMIN_EMAIL: "1cia.rondon@gmail.com"
+    };

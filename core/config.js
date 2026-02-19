b7a39a973a8be2e37602abbf8606364136e34c85
PedroPaesPorto <henriquepaesporto@gmail.com>
2026-02-19 12:53:26 -0400

    Update config.js

--- core/config.js
+++ core/config.js
@@ -1,5 +1,5 @@
 export const CONFIG = {
-        API_URL: "https://script.google.com/macros/s/AKfycbwcW392dz4ktGWfBfSZJ-XlxuQ1gNLd-eSfJAQTzeTGW5kCGJSQ5b0MQL3Fx13jtgkQBw/exec",
+        API_URL: "https://script.google.com/macros/s/AKfycbzGvAESh4s0ywryXDRkBGn2_grAJrHt0h_-jIFpb_JBvx6yxC2iRy8jhMTIkROyk7vqpA/exec",
         VERSAO: "5.0.1",
         EMAIL_LIST: ["gmail.com","hotmail.com","outlook.com","yahoo.com","pm.ro.gov.br"],
         ADMIN_EMAIL: "1cia.rondon@gmail.com"
